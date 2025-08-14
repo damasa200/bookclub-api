af06dc45f29dd0e029a58a727abfd4f6a13659bd
Danilo <danilomartins75@gmail.com>
2025-08-13 23:58:21 -0300

    Corrige uso da chave do Resend

--- src/libs/Mail.js
+++ src/libs/Mail.js
@@ -5,17 +5,12 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 class Mail {
   async sendEmail(email, data,) {
     try {
-      const result = await resend.emails.send({
-        from: 'noreply@send.booclub.damaso.com.br', // você pode trocar depois por seu domínio
-         to: email,
-        subject: 'Recuperação de senha',
-        html: `
-          <h3>Olá, ${data.name}</h3>
-          <p>Use o código abaixo para redefinir sua senha:</p>
-          <h2>${data.token}</h2>
-          <p>Esse código expira em alguns minutos.</p>
-        `,
-      });
+      const data = await resend.emails.send({
+      from: 'noreply@send.booclub.damaso.com.br',
+      to: 'contato@damaso.com.br',
+      subject: 'Teste de envio',
+      html: '<p>Este é um teste de envio usando Resend.</p>',
+    });
 
       return result;
     } catch (error) {
